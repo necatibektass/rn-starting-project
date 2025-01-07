3e9e8203dcbedf89f075ef9610933570182564bd
necati <necatibektass@icloud.com>
2025-01-08 00:05:56 +0300

    Add goal tracking functionality and update dependencies
    Implemented a feature to track course goals with input handling and dynamic display. Enhanced the UI with styling and scrollable goal list. Updated dependencies in package.json and package-lock.json to include react-dom, react-native-web, and other necessary packages.

--- App.js
+++ App.js
@@ -1,12 +1,70 @@
-import {TextInput, View} from 'react-native';
+import {useState} from 'react';
+import {StyleSheet, Button, TextInput, Text, View, ScrollView} from 'react-native';
 
 export default function App() {
+    const [enteredGoalText, setEnteredGoalText] = useState('');
+    const [courseGoals, setCourseGoals] = useState([]);
+
+    function goalInputHandler(enteredText) {
+        setEnteredGoalText(enteredText);
+    }
+
+    function addGoalHandler() {
+        setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalText]);
+    }
+
     return (
-        <View >
-            <View>
-                <TextInput placeholder='Your course goal!' />
+        <View style={styles.appContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalInputHandler}/>
+                <Button title="Add Goal" onPress={addGoalHandler}/>
             </View>
-            <View></View>
+            <View style={styles.goalsContainer}>
+                <ScrollView alwaysBounceVertical={false}>
+                    {courseGoals.map((goal) => (
+                        <View key={goal} style={styles.goalItem}>
+                            <Text style={styles.goalText}>{goal}</Text>
+                        </View>
+                    ))}
+                </ScrollView>
+
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    appContainer: {
+        paddingTop: 50,
+        paddingHorizontal: 16,
+        flex: 1
+    },
+    inputContainer: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: 'center',
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc'
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#cccccc',
+        width: '70%',
+        marginRight: 8,
+        padding: 8
+    },
+    goalsContainer: {
+        flex: 5
+    },
+    goalItem: {
+        margin: 8,
+        padding: 8,
+        borderRadius: 6,
+        backgroundColor: '#5e0acc',
+    },
+    goalText: {
+        color: 'white',
+    },
+});
