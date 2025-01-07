bb5b11d7c8b4ab03b491b1c8b55ef290eb13094a
necati <necatibektass@icloud.com>
2025-01-07 22:34:25 +0300

    Add initial React Native project setup
    This commit introduces the boilerplate code for a React Native project with Expo, including app configuration files, basic assets, and essential dependencies. It also includes `.gitignore` rules, IDE-specific configuration files, and a minimal `App.js` setup.

---
+++ App.js
@@ -0,0 +1,12 @@
+import {TextInput, View} from 'react-native';
+
+export default function App() {
+    return (
+        <View >
+            <View>
+                <TextInput placeholder='Your course goal!' />
+            </View>
+            <View></View>
+        </View>
+    );
+}
