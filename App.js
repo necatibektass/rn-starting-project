27ea7ad5a64559b9619ee44bd869eabf6a822fdf
necat <necati.bektas@rasyona.com>
2025-01-08 15:58:05 +0300

    Refactor and modularize goal input and display components.
    Separated goal input and display logic into `GoalInput` and `GoalItem` components for better code organization and reusability. Replaced `ScrollView` with `FlatList` for optimized rendering of the goals list.

--- App.js
+++ App.js
@@ -1,33 +1,24 @@
 import {useState} from 'react';
-import {StyleSheet, Button, TextInput, Text, View, ScrollView} from 'react-native';
+import {StyleSheet, View, FlatList} from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-    const [enteredGoalText, setEnteredGoalText] = useState('');
     const [courseGoals, setCourseGoals] = useState([]);
 
-    function goalInputHandler(enteredText) {
-        setEnteredGoalText(enteredText);
-    }
-
-    function addGoalHandler() {
-        setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalText]);
+    function addGoalHandler(enteredGoalText) {
+        setCourseGoals(currentCourseGoals => [...currentCourseGoals, {text:enteredGoalText, id: Math.random().toString()}]);
     }
 
     return (
         <View style={styles.appContainer}>
-            <View style={styles.inputContainer}>
-                <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalInputHandler}/>
-                <Button title="Add Goal" onPress={addGoalHandler}/>
-            </View>
+            <GoalInput onAddGoal={addGoalHandler} />
             <View style={styles.goalsContainer}>
-                <ScrollView alwaysBounceVertical={false}>
-                    {courseGoals.map((goal) => (
-                        <View key={goal} style={styles.goalItem}>
-                            <Text style={styles.goalText}>{goal}</Text>
-                        </View>
-                    ))}
-                </ScrollView>
-
+                <FlatList data={courseGoals} alwaysBounceVertical={false} keyExtractor={(item,index)=> {
+                    return item.id;
+                }} renderItem={itemData => {
+                    return <GoalItem text={itemData.item.text} />
+                }}/>
             </View>
         </View>
     );
@@ -39,32 +30,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 16,
         flex: 1
     },
-    inputContainer: {
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: 'center',
-        marginBottom: 24,
-        borderBottomWidth: 1,
-        borderBottomColor: '#cccccc'
-    },
-    textInput: {
-        borderWidth: 1,
-        borderColor: '#cccccc',
-        width: '70%',
-        marginRight: 8,
-        padding: 8
-    },
     goalsContainer: {
         flex: 5
-    },
-    goalItem: {
-        margin: 8,
-        padding: 8,
-        borderRadius: 6,
-        backgroundColor: '#5e0acc',
-    },
-    goalText: {
-        color: 'white',
     },
 });
